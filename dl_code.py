5655833fd758670c556a10a71fc13807e164e1b3
Sarbaroop Ghosh <ghosh.12@iitj.ac.in>
2024-04-04 03:03:09 +0530

    Changed random values

--- dl_code.py
+++ dl_code.py
@@ -2,56 +2,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x)), False
 
 def relu(x):
-    return np.maximum(0, x)
+    return np.maximum(0, x), True
 
 def leaky_relu(x, alpha=0.01):
-    return np.where(x > 0, x, alpha * x)
+    return np.where(x > 0, x, alpha * x), True
 
 def tanh(x):
-    return np.tanh(x)
+    return np.tanh(x), True
 
 # Generate x values from -10 to 10
 x = np.linspace(-10, 10, 100)
 
 # Calculate corresponding y values using the sigmoid function
-y1 = sigmoid(x)
-y2 = relu(x)
-y3 = leaky_relu(x)
-y4 = tanh(x)
 
+y1, boolVal = sigmoid(x)
+y2, boolVal = relu(x)
+y3, boolVal = leaky_relu(x)
+y4, boolVal = tanh(x)
+
+if boolVal:
 # Plot the sigmoid function
-plt.plot(x, y1)
-plt.title('Sigmoid Function')
-plt.xlabel('x')
-plt.ylabel('sigmoid(x)')
-plt.grid(True)
-plt.show
+    plt.plot(x, y1)
+    plt.title('Sigmoid Function')
+    plt.xlabel('x')
+    plt.ylabel('sigmoid(x)')
+    plt.grid(True)
+else:    
+    # Plot the relu function
+    plt.plot(x, y2)
+    plt.title('Relu Function')
+    plt.xlabel('x')
+    plt.ylabel('relu(x)')
+    plt.grid(True)
 
-# Plot the relu function
-plt.plot(x, y2)
-plt.title('Relu Function')
-plt.xlabel('x')
-plt.ylabel('relu(x)')
-plt.grid(True)
-plt.show
+    # Plot the leaky_relu function
+    plt.plot(x, y3)
+    plt.title('leaky_relu Function')
+    plt.xlabel('x')
+    plt.ylabel('leaky_relu(x)')
+    plt.grid(True)
 
-# Plot the leaky_relu function
-plt.plot(x, y3)
-plt.title('leaky_relu Function')
-plt.xlabel('x')
-plt.ylabel('leaky_relu(x)')
-plt.grid(True)
-plt.show
+    # Plot the tanh function
+    plt.plot(x, y4)
+    plt.title('tanh Function')
+    plt.xlabel('x')
+    plt.ylabel('tanh(x)')
+    plt.grid(True)
 
-# Plot the tanh function
-plt.plot(x, y4)
-plt.title('tanh Function')
-plt.xlabel('x')
-plt.ylabel('tanh(x)')
-plt.grid(True)
-plt.show
+    plt.show
 
 
