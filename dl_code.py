dc8633b4a40a7678714b4812af0bb7ebef5ed395
Sarbaroop Ghosh <ghosh.12@iitj.ac.in>
2024-04-04 02:20:05 +0530

    Code for graphs

---
+++ dl_code.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def leaky_relu(x, alpha=0.01):
+    return np.where(x > 0, x, alpha * x)
+
+def tanh(x):
+    return np.tanh(x)
+
+# Generate x values from -10 to 10
+x = np.linspace(-10, 10, 100)
+
+# Calculate corresponding y values using the sigmoid function
+y1 = sigmoid(x)
+y2 = relu(x)
+y3 = leaky_relu(x)
+y4 = tanh(x)
+
+# Plot the sigmoid function
+plt.plot(x, y1)
+plt.title('Sigmoid Function')
+plt.xlabel('x')
+plt.ylabel('sigmoid(x)')
+plt.grid(True)
+plt.show
+
+# Plot the relu function
+plt.plot(x, y2)
+plt.title('Relu Function')
+plt.xlabel('x')
+plt.ylabel('relu(x)')
+plt.grid(True)
+plt.show
+
+# Plot the leaky_relu function
+plt.plot(x, y3)
+plt.title('leaky_relu Function')
+plt.xlabel('x')
+plt.ylabel('leaky_relu(x)')
+plt.grid(True)
+plt.show
+
+# Plot the tanh function
+plt.plot(x, y4)
+plt.title('tanh Function')
+plt.xlabel('x')
+plt.ylabel('tanh(x)')
+plt.grid(True)
+plt.show
+
+
